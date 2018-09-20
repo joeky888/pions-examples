f50e48e779da2a8be37a3f759728bd6dcf0c6fbf
Joeky Zhan <jj16180339887@gmail.com>
2018-09-20 22:40:54 +0800

    Init
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+#### Dependencies for alsa (Linux only):
+
+`sudo apt install libasound2-dev libopus-dev libopusfile-dev`
+
+#### Dependencies for portaudio (Cross-platform):
+
+Linux:
+
+`sudo apt install libportaudio-dev libopus-dev libopusfile-dev`
+
+Mac:
+
+`brew install portaudio opus opusfile`
+
+
+Licence of these examples: Public domain/Unlicensed
+
---
+++ alsa-receive/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"bufio"
+	"encoding/base64"
+
+	"github.com/pions/webrtc"
+	// "github.com/pions/webrtc/examples/gstreamer-receive/gst"
+	"github.com/cocoonlife/goalsa"
+	"github.com/pions/webrtc/pkg/ice"
+	"gopkg.in/hraban/opus.v2"
+)
+
+var ctrlc = make(chan os.Signal)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	rawSd, err := reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
+	fmt.Println("")
+	sd, err := base64.StdEncoding.DecodeString(rawSd)
+	if err != nil {
+		panic(err)
+	}
+
+	/* Everything below is the pion-WebRTC API, thanks for using it! */
+
+	// Setup the codecs you want to use.
+	// We'll use the default ones but you can also define your own
+	webrtc.RegisterDefaultCodecs()
+
+	// Create a new RTCPeerConnection
+	peerConnection, err := webrtc.New(webrtc.RTCConfiguration{
+		IceServers: []webrtc.RTCIceServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	const channels = 1
+	const sampleRate = 16000
+	const format = alsa.FormatS16LE
+	const pcmsize = 320
+
+	player, err := alsa.NewPlaybackDevice("default", channels, format, sampleRate,
+		alsa.BufferParams{BufferFrames: 0, PeriodFrames: pcmsize, Periods: pcmsize})
+	if err != nil {
+		panic(err)
+	}
+	defer player.Close()
+	// signal.Notify(ctrlc, os.Interrupt, syscall.SIGTERM)
+	// go cleanup(player)
+
+	// Set a handler for when a new remote track starts, this handler creates a gstreamer pipeline
+	// for the given codec
+	peerConnection.OnTrack = func(track *webrtc.RTCTrack) {
+		codec := track.Codec
+		fmt.Printf("Track has started, of type %d: %s \n", track.PayloadType, codec.Name)
+		// pipeline := gst.CreatePipeline(codec.Name)
+		// pipeline.Start()
+		pcm := make([]int16, pcmsize)
+
+		dec, err := opus.NewDecoder(sampleRate, channels)
+		if err != nil {
+			panic(err)
+		}
+
+		for {
+			p := <-track.Packets
+
+			_, err := dec.Decode(p.Payload, pcm)
+			if err != nil {
+				panic(err)
+			}
+
+			player.Write(pcm)
+			// fmt.Println(pcm)
+		}
+	}
+
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange = func(connectionState ice.ConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	}
+
+	// Set the remote SessionDescription
+	offer := webrtc.RTCSessionDescription{
+		Type: webrtc.RTCSdpTypeOffer,
+		Sdp:  string(sd),
+	}
+	if err := peerConnection.SetRemoteDescription(offer); err != nil {
+		panic(err)
+	}
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Get the LocalDescription and take it to base64 so we can paste in browser
+	fmt.Println(base64.StdEncoding.EncodeToString([]byte(answer.Sdp)))
+
+	select {}
+}
+
+// func cleanup(player *alsa.PlaybackDevice) {
+// 	// User hit Ctrl-C, clean up
+// 	<-ctrlc
+// 	fmt.Println("Close devices")
+// 	player.Close()
+// 	os.Exit(1)
+// }
